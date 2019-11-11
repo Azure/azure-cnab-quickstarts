e5064e6e241cc560e45905b48ea537a84523dff7
Mike Larah <mike.larah@endjin.com>
2019-11-11 13:52:39 +0000

    Fix if statement

--- porter/aks/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $(az aks show --name blah --resource-group blah) == false ]]
-then
-    echo "Hello!"
-else
-    echo "AKS cluster already exists in specified resource group with specified name"
-fi
