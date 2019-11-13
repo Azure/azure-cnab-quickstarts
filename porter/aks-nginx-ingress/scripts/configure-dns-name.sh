b55456fdddbefc0fd741b2883c09c4fde7246c22
Mike Larah <mike.larah@endjin.com>
2019-11-13 16:00:01 +0000

    Add fqdn as output

--- porter/aks-nginx-ingress/scripts/configure-dns-name.sh
+++ porter/aks-nginx-ingress/scripts/configure-dns-name.sh
@@ -11,7 +11,7 @@ while [ -z $IP ]; do
         exit 1    
     fi
 
-    IP=$(kubectl get service -l app=$INSTALLATION_NAME --namespace $NAMESPACE -o=jsonpath='{.items[0].status.loadBalancer.ingress[*].ip}')
+    IP=$(kubectl get service -l app=nginx-ingress,release=$INSTALLATION_NAME --namespace $NAMESPACE -o=jsonpath='{.items[0].status.loadBalancer.ingress[*].ip}')
 
     if [[ -z $IP ]]; then
         echo "External IP address not ready yet. Trying again in 5 seconds."
@@ -25,3 +25,5 @@ done
 DNSNAME=$DNS_NAME
 PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[id]" --output tsv)
 az network public-ip update --ids $PUBLICIPID --dns-name $DNSNAME
+FQDN=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[dnsSettings.fqdn]" --output tsv)
+echo $FQDN > FQDN
