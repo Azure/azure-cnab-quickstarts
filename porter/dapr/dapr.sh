5e1c19a509da8481bb2d1453a4db5ef22f4338d8
Mike Larah <mike.larah@endjin.com>
2020-04-07 17:46:56 +0100

    Remove options

--- porter/dapr/dapr.sh
+++ porter/dapr/dapr.sh
@@ -1,8 +1,3 @@
-function replace-tokens() {
-    envsubst < /cnab/app/values.yaml.template > /cnab/app/values.yaml
-    cat /cnab/app/values.yaml && echo \n\n
-}
-     
 function create-kubernetes-namespace() {
     kubectl create namespace $1
 }
